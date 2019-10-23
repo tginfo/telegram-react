ec3a487b5bb5ac57d160563db90cdf1b06f32993
RadRussianRus <radrussianrus@gmail.com>
2019-10-23 06:43:21 +0000

    Updates

---
+++ precache-manifest.9cf79fcceeab181721d17a7e6a2029c7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9800d3407ece96e3248b1447f3ec3755",
+    "url": "/69a5dd09789a7c86d44b.worker.js"
+  },
+  {
+    "revision": "3b811d61a73978742d5b46571f26d1eb",
+    "url": "/db524e4eb064795d94a5.worker.js"
+  },
+  {
+    "revision": "71b22ed02529f13211248dc99854ac1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b0c04ef7d272efc775d",
+    "url": "/static/css/4.20fe2f3c.chunk.css"
+  },
+  {
+    "revision": "ea57058b0f22856bad8a",
+    "url": "/static/css/5.0437bcf7.chunk.css"
+  },
+  {
+    "revision": "bfe6f2dc3c1f3fa0c39d",
+    "url": "/static/css/main.91a5cadf.chunk.css"
+  },
+  {
+    "revision": "d0849bf6dfaffde5e317",
+    "url": "/static/js/2.dbd7c855.chunk.js"
+  },
+  {
+    "revision": "67bbd7e616a4bbb0c079",
+    "url": "/static/js/3.b36b1fb2.chunk.js"
+  },
+  {
+    "revision": "2b0c04ef7d272efc775d",
+    "url": "/static/js/4.2b7f17f4.chunk.js"
+  },
+  {
+    "revision": "ea57058b0f22856bad8a",
+    "url": "/static/js/5.05fae4b7.chunk.js"
+  },
+  {
+    "revision": "9c7e1f19e3f43eb85df1",
+    "url": "/static/js/6.8ba38e6c.chunk.js"
+  },
+  {
+    "revision": "bfe6f2dc3c1f3fa0c39d",
+    "url": "/static/js/main.f161e3cc.chunk.js"
+  },
+  {
+    "revision": "3df25de5c1648159f9ba",
+    "url": "/static/js/runtime~main.a7030748.js"
+  },
+  {
+    "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
+    "url": "/static/media/Bubbles.5b7ac6f8.svg"
+  },
+  {
+    "revision": "7a71629a5e4f7482b6320b103f1d543c",
+    "url": "/static/media/Manytabs_2x.7a71629a.png"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/static/media/check.71da7469.svg"
+  }
+]);
