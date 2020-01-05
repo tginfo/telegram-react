5ca20db1d4ec6d4342f874e5104915a07c965930
RadRussianRus <radrussianrus@gmail.com>
2020-01-05 16:09:32 +0000

    Updates

---
+++ precache-manifest.d75539bfac324440e88caf2d0c7c0bad.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4dcd721d6b397a670b239b4be6d68d2",
+    "url": "/b7811fdb7f717e14d151.worker.js"
+  },
+  {
+    "revision": "e54fdea981ad3c712d07e94e26132a48",
+    "url": "/bb47406301bca11a5d8d.worker.js"
+  },
+  {
+    "revision": "0fa39e86f951641b78d627dd733a58a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5e3ecf9f1174be357b2",
+    "url": "/static/css/4.8a6a59d3.chunk.css"
+  },
+  {
+    "revision": "81aaf74065edc96699b4",
+    "url": "/static/css/main.8fab1f57.chunk.css"
+  },
+  {
+    "revision": "e05d82c35fb57628aca9",
+    "url": "/static/js/2.d57c5e4c.chunk.js"
+  },
+  {
+    "revision": "4d58be5d8f8a3e8252db98545f038199",
+    "url": "/static/js/2.d57c5e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b047e737bb955939d6c",
+    "url": "/static/js/3.ce968014.chunk.js"
+  },
+  {
+    "revision": "c5e3ecf9f1174be357b2",
+    "url": "/static/js/4.50cdd409.chunk.js"
+  },
+  {
+    "revision": "81aaf74065edc96699b4",
+    "url": "/static/js/main.cd20e636.chunk.js"
+  },
+  {
+    "revision": "fd2ac0ada20497ec66fc",
+    "url": "/static/js/runtime-main.e3f9a69c.js"
+  },
+  {
+    "revision": "5b7ac6f843c4f6909eaf8d7c07179ebd",
+    "url": "/static/media/Bubbles.5b7ac6f8.svg"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
