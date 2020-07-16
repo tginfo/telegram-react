a1be4d44039131c2bd2627217e1e1bef353194fd
RadRussianRus <radrussianrus@gmail.com>
2020-07-16 20:06:34 +0000

    Updates

---
+++ precache-manifest.e2fda80d0edf660752a0fcf93ab42634.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b811d61a73978742d5b46571f26d1eb",
+    "url": "/db524e4eb064795d94a5.worker.js"
+  },
+  {
+    "revision": "5fb4ffc525c1d951ddd2ddb6e8e139b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b257151995648a26c7",
+    "url": "/static/css/0.885200b4.chunk.css"
+  },
+  {
+    "revision": "de0f6a2a45537c91aded",
+    "url": "/static/css/4.a767afb1.chunk.css"
+  },
+  {
+    "revision": "4ce8033217a2b0198235",
+    "url": "/static/css/6.6ede3073.chunk.css"
+  },
+  {
+    "revision": "6520207180367fdb0fdb",
+    "url": "/static/css/8.3015292a.chunk.css"
+  },
+  {
+    "revision": "2c442d91ef52eba43075",
+    "url": "/static/css/main.205ac9f6.chunk.css"
+  },
+  {
+    "revision": "34b257151995648a26c7",
+    "url": "/static/js/0.e2a22281.chunk.js"
+  },
+  {
+    "revision": "984edcf17e82b11e437e",
+    "url": "/static/js/1.bd163751.chunk.js"
+  },
+  {
+    "revision": "de0f6a2a45537c91aded",
+    "url": "/static/js/4.906e9c6f.chunk.js"
+  },
+  {
+    "revision": "f6b2bc9eaffa9a73782a",
+    "url": "/static/js/5.75f2b0af.chunk.js"
+  },
+  {
+    "revision": "4ce8033217a2b0198235",
+    "url": "/static/js/6.d8b69d10.chunk.js"
+  },
+  {
+    "revision": "d27f459b71215eebf63e",
+    "url": "/static/js/7.9fdd1fde.chunk.js"
+  },
+  {
+    "revision": "6520207180367fdb0fdb",
+    "url": "/static/js/8.ef4d6699.chunk.js"
+  },
+  {
+    "revision": "2c442d91ef52eba43075",
+    "url": "/static/js/main.d16e9cec.chunk.js"
+  },
+  {
+    "revision": "98bf79bb35ee415da3de",
+    "url": "/static/js/runtime~main.19c33a70.js"
+  },
+  {
+    "revision": "9b7ea631ddb3103beee05cec006af20e",
+    "url": "/static/media/bg.9b7ea631.jpg"
+  },
+  {
+    "revision": "cbb4eead04871188de6ce488808e4237",
+    "url": "/static/media/bubble-tail-left.cbb4eead.svg"
+  },
+  {
+    "revision": "b159f8b04a1646f68bfc0bfb7f347cac",
+    "url": "/static/media/bubble-tail-right.b159f8b0.svg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
